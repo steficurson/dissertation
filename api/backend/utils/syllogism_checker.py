1dcb72ff9885d7b3744913d134aa47ef3a6545a3
steficurson <s2172960@ed.ac.uk>
2025-03-21 01:13:30 +0000

    aaaaaa

--- api/backend/utils/syllogism_checker.py
+++ api/backend/utils/syllogism_checker.py
@@ -2,7 +2,7 @@ from utils.syllogism import Syllogism
 from utils.answer import Answer
 
 
-def check_answer(sectionState, lineState, selectedAnswer, json_syllogism):
+def check_answer(section_state, line_state, selected_answer, json_syllogism):
     syllogism = Syllogism(json_syllogism)
 
     #Takes the text of the syllogism and parses it into a Syllogism object,
@@ -12,7 +12,7 @@ def check_answer(sectionState, lineState, selectedAnswer, json_syllogism):
     #Initialise the states of our 'correct answer'
     answer = Answer()
 
-    #Codify middle as A, major as B, minor as C, to match the sectionState keys
+    #Codify middle as A, major as B, minor as C, to match the section_state keys
     MIDDLE = "A"
     MAJOR = "B"
     MINOR = "C"
@@ -34,16 +34,16 @@ def check_answer(sectionState, lineState, selectedAnswer, json_syllogism):
     answer.resolve_inconsistencies()
 
     #Check if the student's answer matches the correct answer
-    incorrectSections = {}
-    incorrectLines = {}
-    for section, state in sectionState.items():
+    incorrect_sections = {}
+    incorrect_lines = {}
+    for section, state in section_state.items():
         if state.get('state') != answer.section_state[section].value:
-            incorrectSections[section] = {"expected": answer.section_state[section].value, "actual": state.get('state')}
-    for line, state in lineState.items():
+            incorrect_sections[section] = {"expected": answer.section_state[section].value, "actual": state.get('state')}
+    for line, state in line_state.items():
         if state.get('state') != answer.line_state[line].value:
-            incorrectLines[line] = {"expected": answer.line_state[line].value, "actual": state.get('state')}
+            incorrect_lines[line] = {"expected": answer.line_state[line].value, "actual": state.get('state')}
 
-    return {"main_answer_correct": syllogism.is_valid() == selectedAnswer,
-        "incorrectSections": incorrectSections,
-        "incorrectLines": incorrectLines
+    return {"main_answer_correct": syllogism.is_valid() == selected_answer,
+        "incorrect_sections": incorrect_sections,
+        "incorrect_lines": incorrect_lines
     }
